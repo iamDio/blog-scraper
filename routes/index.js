9a1e27bd347a0a1602cda6ceca34825264d658f4
iamDio <claudio.villani00@gmail.com>
2017-09-04 14:54:40 -0400

    added routes folder and files

---
+++ routes/index.js
@@ -0,0 +1,81 @@
+const express = require('express');
+
+const logger = require('morgan');
+//let mongojs = require('mongojs');
+const mongoose=require('mongoose');
+// Require request and cheerio. This makes the scraping possible
+const request= require('request');
+const cheerio= require('cheerio');
+// Requiring our Note and Article models
+const Note = require("../models/Note.js");
+const News = require("../models/News.js");
+
+const router = express.Router();
+
+// Main route, should render index.handlebars
+router.get('/', function(req,res){
+	res.render('index')
+});
+
+//write a function that checks for errors
+//@TODO: rewrite this code using async/await and/or promises
+
+router.get('/scrape', function(req,res){
+	request('https://medium.com/', function(error, response, html) {
+		  if (!error && response.statusCode == 200) {
+		    let $ = cheerio.load(html);
+		    
+		 $('h3.u-contentSansBold').each(function(i, element){
+		//$('article.h3').each(function(i, element) {
+			
+			let result = {};
+			result.title = $(this).text();
+			result.text = $(this).parent('a').text();
+			result.link = $(this).parent('a').attr('href');
+
+			console.log(result)
+			
+
+		let entry = new News(result);
+
+			entry.save(function(err,doc){
+				if(err) {
+					console.log(err);
+				}else{
+					console.log(doc);
+				}
+			})
+			})
+		  }
+
+	//res.render('index', {article:News});
+	})
+	
+	res.send('scrape complete');
+})
+
+//retrieves from the db all of the articles scraped 
+router.get('/articles', function(req, res){
+	News.find({}, function(error,doc){
+		if(error) {
+			console.log(error);
+		}else{
+			res.json(doc);
+		}
+	});
+});
+
+//retrieves from the db a specific article
+router.get('/articles/:id', function(req,res){
+	News.findOne({_id: req.params.id})
+		.populate('')
+		.exec(function(error,doc) {
+		if(error) {
+			console.log(error);
+		}else{
+			res.json(doc);
+			}
+		});
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -12,6 +12,8 @@ const cheerio= require('cheerio');
 const Note = require("./models/Note.js");
 const News = require("./models/News.js");
 
+//require routes file 
+const routes = require('./routes/index')
 
 mongoose.Promise = Promise;
 
@@ -29,6 +31,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 //make views a static component
 app.use(express.static('views'));
 
+app.use('/', routes);
 // set up mongoose connection
 mongoose.connect('mongodb://localhost/articleScraper');
 let db = mongoose.connection;
@@ -42,74 +45,6 @@ db.on('error', function(error){
 db.once('open', function(){
 	console.log('Mongoose connection succesful');
 });
-
-
-// Main route, should render index.handlebars
-app.get('/', function(req,res){
-	res.render('index')
-});
-
-//write a function that checks for errors
-//@TODO: rewrite this code using async/await and/or promises
-
-app.get('/scrape', function(req,res){
-	request('https://medium.com/', function(error, response, html) {
-		  if (!error && response.statusCode == 200) {
-		    let $ = cheerio.load(html);
-		    
-		 $('h3.u-contentSansBold').each(function(i, element){
-		//$('article.h3').each(function(i, element) {
-
-
-
-			let result = {};
-			result.title = $(this).text();
-			result.text = $(this).parent('a').text();
-			result.link = $(this).parent('a').attr('href');
-
-			console.log(result)
-		let entry = new News(result);
-
-			entry.save(function(err,doc){
-				if(err) {
-					console.log(err);
-				}else{
-					console.log(doc);
-				}
-			})
-			})
-		  }
-
-	res.send('scrape complete');
-	})
-
-})
-
-//retrieves from the db all of the articles scraped 
-app.get('/articles', function(req, res){
-	News.find({}, function(error,doc){
-		if(error) {
-			console.log(error);
-		}else{
-			res.json(doc);
-		}
-	});
-});
-
-//retrieves from the db a specific article
-app.get('/articles/:id', function(req,res){
-	News.findOne({_id: req.params.id})
-		.populate('')
-		.exec(function(error,doc) {
-		if(error) {
-			console.log(error);
-		}else{
-			res.json(doc);
-			}
-		});
-});
-
-
 
 // Listen on port 3000
 app.listen(PORT, () => console.log(`app running on port: ${PORT}`));
